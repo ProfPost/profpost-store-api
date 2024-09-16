43bdb7e7f3d39cd4c5f032e6b6524810de541272
oscarllc <ollaurec1@upao.edu.pe>
2024-09-15 22:06:34 -0500

    feat(backend) Implementacion del CRUD de category

---
+++ src/main/java/com/Profpost/api/CategoryController.java
@@ -0,0 +1,50 @@
+package com.Profpost.api;
+
+import com.Profpost.model.entity.Category;
+import com.Profpost.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import java.util.List;
+
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories(){
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable("id") Integer id){
+        Category category = categoryService.findById(id);
+        return new ResponseEntity<Category>(category, HttpStatus.OK);
+
+    }
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        Category newCategory = categoryService.Create(category);
+        return new ResponseEntity<Category>(newCategory,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Integer id, @RequestBody Category category){
+        Category updateCategory = categoryService.Update(id, category);
+        return new ResponseEntity<Category>(updateCategory,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable("id") Integer id){
+        categoryService.delete(id);
+        return new ResponseEntity<Category>(HttpStatus.NO_CONTENT);
+
+    }
+
+}

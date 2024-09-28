d54764aaaf9ce05d06a17c00e095ebd52fc31570
VaniaR <vramosc2@upao.edu.pe>
2024-09-28 16:06:43 -0500

    feat(backend): se agregó validación, control de excepciones y patron DTO a CRUD plan

--- src/main/java/com/Profpost/config/ModelMapper.java
+++
@@ -1,7 +0,0 @@
-package com.Profpost.config;
-
-public class ModelMapper {
-    void hello(){
-        System.out.println("Hello from ModelMapper");
-    };
-}
--- src/main/java/com/Profpost/service/impl/PlanServiceImpl.java
+++ src/main/java/com/Profpost/service/impl/PlanServiceImpl.java
@@ -1,5 +1,9 @@
 package com.Profpost.service.impl;
 
+import com.Profpost.dto.PlanDTO;
+import com.Profpost.exception.ResourceNotFoundException;
+import com.Profpost.exception.BadRequestException;
+import com.Profpost.mapper.PlanMapper;
 import com.Profpost.model.entity.Plan;
 import com.Profpost.repository.PlanRepository;
 import com.Profpost.service.PlanService;
@@ -14,40 +18,60 @@ import java.util.List;
 
 public class PlanServiceImpl implements PlanService {
     private final PlanRepository planRepository;
+    private final PlanMapper planMapper;
 
     @Transactional(readOnly = true)
     @Override
-    public List<Plan> findAll() {
-        return planRepository.findAll();
+    public List<PlanDTO> findAll() {
+        List<Plan> plans = planRepository.findAll();
+        return plans.stream()
+                .map(planMapper::toPlanDTO)
+                .toList();
     }
 
     @Transactional
     @Override
-    public Plan create(Plan plan) {
-        return planRepository.save(plan);
+    public PlanDTO create(PlanDTO planDTO) {
+        planRepository.findByName(planDTO.getName())
+                .ifPresent(existingPlan -> {
+                    throw new BadRequestException("Plan already exists");
+                });
+        Plan plan = planMapper.toPlanEntity(planDTO);
+        Plan savedPlan = planRepository.save(plan);
+        return planMapper.toPlanDTO(savedPlan);
     }
 
     @Transactional(readOnly = true)
     @Override
-    public Plan findById(Integer id) {
-        return planRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Plan not found"));
+    public PlanDTO findById(Integer id) {
+        Plan plan = planRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("EL Plan with Id " +id+ "not found"));
+        return planMapper.toPlanDTO(plan);
     }
 
     @Transactional
     @Override
-    public Plan update(Integer id, Plan updatedPlan) {
-        Plan planFromDb = findById(id);
-        planFromDb.setName(updatedPlan.getName());
-        planFromDb.setPrice(updatedPlan.getPrice());
-        planFromDb.setDescription(updatedPlan.getDescription());
-        return planRepository.save(planFromDb);
+    public PlanDTO update(Integer id, PlanDTO planDTO) {
+        Plan plan = planRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("EL Plan with Id " +id+ "not found"));
+        planRepository.findByName(planDTO.getName())
+                .ifPresent(existingPlan -> {
+                    throw new BadRequestException("Ya existe un plan con el mismo nombre");
+                });
+
+        plan.setName(planDTO.getName());
+        plan.setPrice(planDTO.getPrice());
+        plan.setDescription(planDTO.getDescription());
+
+        Plan updatedPlan = planRepository.save(plan);
+        return planMapper.toPlanDTO(updatedPlan);
     }
 
     @Transactional
     @Override
     public void delete(Integer id) {
-        Plan plan = findById(id);
+        Plan plan = planRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("EL Plan with Id " +id+ "not found"));
         planRepository.delete(plan);
     }
 

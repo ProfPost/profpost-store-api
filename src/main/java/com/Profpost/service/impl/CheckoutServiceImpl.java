3a9cbc2d2676ae9f25cdf4e9d101fbf9b4aff0d6
Sergio <svelasquezr1@gmail.com>
2024-10-22 15:18:12 -0500

    feat(backend) actualización de diseño de envíos de correos

--- src/main/java/com/Profpost/service/impl/CheckoutServiceImpl.java
+++ src/main/java/com/Profpost/service/impl/CheckoutServiceImpl.java
@@ -3,21 +3,32 @@ package com.Profpost.service.impl;
 import com.Profpost.dto.PaymentCaptureResponse;
 import com.Profpost.dto.PaymentOrderResponse;
 import com.Profpost.dto.PurchaseDTO;
+import com.Profpost.integration.notification.email.dto.Mail;
+import com.Profpost.integration.notification.email.service.EmailService;
 import com.Profpost.integration.payment.paypal.dto.OrderCaptureResponse;
 import com.Profpost.integration.payment.paypal.dto.OrderResponse;
 import com.Profpost.integration.payment.paypal.service.PaypalService;
-import com.Profpost.model.entity.Subscription;
-import com.Profpost.model.enums.SubscriptionState;
 import com.Profpost.service.CheckoutService;
 import com.Profpost.service.PurchaseService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
     private final PaypalService paypalService;
     private final PurchaseService purchaseService;
+    private final EmailService emailService;
+
+    @Value("${spring.mail.username}")
+    private String mailFrom;
 
     @Override
     public PaymentOrderResponse createPayment(Integer purchaseId, String returnUrl, String cancelUrl) {
@@ -35,7 +46,7 @@ public class CheckoutServiceImpl implements CheckoutService {
     }
 
     @Override
-    public PaymentCaptureResponse capturePayment(String orderId) {
+    public PaymentCaptureResponse capturePayment(String orderId) throws MessagingException {
         OrderCaptureResponse orderCaptureResponse = paypalService.captureOrder(orderId);
         boolean completed = orderCaptureResponse.getStatus().equals("COMPLETED");
 
@@ -46,8 +57,27 @@ public class CheckoutServiceImpl implements CheckoutService {
             String purchaseIdStr = orderCaptureResponse.getPurchaseUnits().get(0).getReferenceId();
             PurchaseDTO purchaseDTO = purchaseService.confirmPurchase(Integer.parseInt(purchaseIdStr));
             paypalCaptureResponse.setPurchaseId(purchaseDTO.getId());
+            sendPurchaseConfirmationEmail(purchaseDTO);
         }
 
         return paypalCaptureResponse;
+    }
+
+    private void sendPurchaseConfirmationEmail(PurchaseDTO purchaseDTO) throws MessagingException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("user", email);
+        model.put("total", purchaseDTO.getTotal());
+        model.put("orderUrl", "http://localhost:4200/order/" + purchaseDTO.getId());
+
+        Mail mail = emailService.createMail(
+                email,
+                "Confirmación de Pago",
+                model,
+                mailFrom
+        );
+        emailService.sendMail(mail, "email/purchase-confirmation-template");
     }
 }
